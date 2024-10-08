952466538ede7db707558c6213a52326a9cf80b6
ABHISHEK KUMAR NISHAD <147787726+abhibbolt@users.noreply.github.com>
2024-10-08 19:03:07 +0000

    datatype

---
+++ 02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JS code as newer version
+
+//  alert(3+3)// we are usibg  node.js not browser.
+
+console.log(3+2);
+console.log("abhishek");
+
+// code readability should high....!
+
+// datatypes in js
+
+let name = "abhsihek"
+let age = 12
+let isLoggedIN = false
+
+//number => 2 to power 52
+//bigint
+//string
+//boolean=>true/false
+// null=>standalone value 
+// undefined=>not assign value
+// symbol=>unique
+
+//object
+
+console.log();
+
+
