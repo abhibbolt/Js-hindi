cd83ab462cf9b24e848e30d38f433fa0bdeab17a
ABHISHEK KUMAR NISHAD <147787726+abhibbolt@users.noreply.github.com>
2024-10-08 18:48:47 +0000

    datatypes

---
+++ js-basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 124424
+let accountEmail = "anhis@gmail.com"
+var accountPassword = "13412"
+accountCity = "jaipur"
+
+//accountId = 2
+accountEmail = "abhishek@gmail.com"
+accountPassword  = "33333"
+accountCity  = "kanpur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
