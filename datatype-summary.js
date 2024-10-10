a928c8b0e5508f64aed03f1c83660a5db8e9037f
ABHISHEK KUMAR NISHAD <147787726+abhibbolt@users.noreply.github.com>
2024-10-10 17:28:44 +0000

    datatype-sumary

---
+++ datatype-summary.js
@@ -0,0 +1,26 @@
+// primitive
+
+// 7 types:  String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+const id  = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id==anotherId)
+
+
+// reference (Non-primitine)
+
+// Array, Object, Function
+
+const herso = ["hitman","shaktiman","doga"]
+let object={
+    name:abhi,
+    age:22,
+}
+
+const myFunction = function(){
+    const myFunction = function(){
+        console.log("hello abhi")
+    }
+}
