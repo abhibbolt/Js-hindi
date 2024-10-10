1120ffc41ca202bcdb52057c6042a043ba3c6597
ABHISHEK KUMAR NISHAD <147787726+abhibbolt@users.noreply.github.com>
2024-10-10 18:19:48 +0000

    stack

---
+++ js-basic/stack-memory.js
@@ -0,0 +1,21 @@
+
+// Stach(primitive) , Heap(Non-primitive)
+ 
+let myYoutube = "abhishek"
+let anotherName = myYoutube
+anotherName = "chaiaurcode"
+//console.log(myYoutube);
+//console.log(anotherName);
+
+let userOne = {
+    email:"user@gmail.com",
+    upi:"user@upi"
+}
+let userTwo = userOne
+userTwo.email = "Abhi@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email)
+// heap we change value in original value not in referance
+
+
+
