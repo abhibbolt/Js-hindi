8d249655791fdaa9a3de0e69db554de8bc71e03f
ABHISHEK KUMAR NISHAD <147787726+abhibbolt@users.noreply.github.com>
2024-10-11 08:17:07 +0000

    math

---
+++ js-basic/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const otherNumber = 123.2323
+// console.log(otherNumber.toPrecision(2));
+
+// const hundred = 100000
+// console.log(hundred.toLocaleString('en-IN'));
+
+// ************ Math**********//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.44));
+// console.log((Math.ceil(4.6)));
+
+
+console.log(Math.random());
+console.log(Math.random()*10);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max-min+1)) +min )
+
+
+
+
+
+
+
+
