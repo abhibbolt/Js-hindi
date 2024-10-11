a6f509d11ace730802dfe2bd9bc8393bced2e5e2
ABHISHEK KUMAR NISHAD <147787726+abhibbolt@users.noreply.github.com>
2024-10-11 06:20:50 +0000

    strings

---
+++ js-basic/05_string.js
@@ -0,0 +1,19 @@
+const name = "abhishek"
+const repoCount = 50
+
+//console.log(name + repocount + "value") // not  use in modern days
+
+console.log(`helloji my name is ${name} and my repo is ${repoCount}`);
+
+const gameName = new String('abhishek')
+console.log(gameName[1]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+
+
+
+
