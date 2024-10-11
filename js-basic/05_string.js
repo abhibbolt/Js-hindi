268c38d0f44446906cbc3317bd008269ef9f9eb8
ABHISHEK KUMAR NISHAD <147787726+abhibbolt@users.noreply.github.com>
2024-10-11 06:54:29 +0000

    string01

--- js-basic/05_string.js
+++ js-basic/05_string.js
@@ -5,13 +5,31 @@ const repoCount = 50
 
 console.log(`helloji my name is ${name} and my repo is ${repoCount}`);
 
-const gameName = new String('abhishek')
+const gameName = new String('abhishek-bb')
 console.log(gameName[1]);
 console.log(gameName.__proto__);
 
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
+console.log(gameName.indexOf('i'))
+
+const newString = gameName.substring(0,4)
+console.log(newString); // not include last charactor ir a(whicj is index of 4)
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString); // it print chartor at index 3
+
+const newStringOne = "    abhishek   "
+console.log(newStringOne)
+console.log(newStringOne.trim()); // it Eliminate the space
+
+const url = "https://abhishek.com/abhi%20bbolt/"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('bbolt'));
+
+
 
 
 
