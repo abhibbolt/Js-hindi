3dcd47c81737e52e9561f33cbf3a453144d1f36d
ABHISHEK KUMAR NISHAD <147787726+abhibbolt@users.noreply.github.com>
2024-10-14 12:27:06 +0000

    objects.

---
+++ 04_object.js
@@ -0,0 +1,52 @@
+// singelton object declaration
+// const Tinderuser = new Object()
+// console.log(Tinderuser);
+
+// non-singelton object
+
+const tinderUser = {}
+
+tinderUser.id = "123wss"
+tinderUser.name = "samay"
+tinderUser.isloggged = false
+
+//console.log(tinderUser);
+
+const regularUser = {
+    email:"abhi@gmail.com",
+    fullname:{
+        username:{
+            firstname:"abhishek",
+            lastname:"kumar"
+        }
+    }
+}
+//console.log(regularUser.fullname.username.firstname);
+const obj1 = {1:"q",2:"a"}
+const obj2 = {3:"e",4:"r"}
+
+//const obj3 = {...obj1,...obj2}
+const obj3 = Object.assign(obj1,obj2)
+//console.log(obj3);
+
+const user = [
+    {
+        id:1,
+        email:"abhi@gmail.com"
+    },
+    {
+
+    },
+]
+ user[1].email
+ console.log(tinderUser);
+
+ console.log(Object.keys(tinderUser));
+ console.log(Object.values(tinderUser));
+ 
+ 
+
+
+
+
+ 
