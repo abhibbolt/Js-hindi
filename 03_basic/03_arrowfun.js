e055135deef75010b10a511695c08dc98d34082c
ABHISHEK KUMAR NISHAD <147787726+abhibbolt@users.noreply.github.com>
2024-10-15 18:14:46 +0000

    arrows

---
+++ 03_basic/03_arrowfun.js
@@ -0,0 +1,52 @@
+/// arrow function
+
+const user ={
+    username:"abhi",     // this.=>refers current context
+    price:999,
+
+    welcomeMessage:function(){
+        console.log(`${this.username} , welcome to website `);
+        console.log(this);
+        
+        
+    }
+
+
+}
+
+// user.welcomeMessage()
+// user.username = "change"
+// user.welcomeMessage()
+
+//console.log(this);// it print empty element i.e {}
+
+
+// function chai(){
+//     let username= "abhi"
+//     console.log(this.username);//undefined is print
+    
+// }
+
+// chai()
+
+// const chai = () => {
+//     let username = "abhi"
+//     console.log(this);
+
+// }
+// chai() is will also show empty function 
+
+
+// arrow function
+// const addtwo=(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(addtwo(2,3));
+
+// const addtwo1=(num1,num2)=> num1+num2
+// console.log(addtwo1(1,1));
+
+const addtwo1 = (num1, num2) => (num1 + num2)
+console.log(addtwo1(1, 1));
+
+
