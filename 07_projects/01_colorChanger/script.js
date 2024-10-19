4b2dc939074589667ed3a4d7980b1f57c762a1d6
ABHISHEK KUMAR NISHAD <147787726+abhibbolt@users.noreply.github.com>
2024-10-19 18:55:27 +0000

    p1

--- 07_projects/01_colorChanger/script.js
+++ 07_projects/01_colorChanger/script.js
@@ -1,5 +1,5 @@
 //console.log("abhishek");
-document.querySelectorAll('.button');
+const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body')
 
 buttons.forEach(function (button){
@@ -10,6 +10,15 @@ buttons.forEach(function (button){
         
         
         if(e.target.id==='grey'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'yellow') {
             body.style.backgroundColor = e.target.id;
         }
     })
